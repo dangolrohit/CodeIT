b499d2d7b715d931750823da69dfcf461736f14f
Rohit Dangol <rohit.god44455@gmail.com>
2024-02-24 18:35:13 +0545

    login modified

--- src/components/Loginarea.jsx
+++ src/components/Loginarea.jsx
@@ -1,19 +1,42 @@
 import React from "react";
 import "./login.css";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const Loginarea = () => {
-  let btn = document.getElementById("btn");
+  const nav = useNavigate();
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+    const email = e.target.elements.email.value;
+    const passWord = e.target.elements.password.value;
+    if (email === "admin" && passWord === "admin") {
+      nav("/dashboard");
+    } else {
+      alert("Sorry credential did not match");
+    }
+  };
+
   return (
     <div>
       <div className="loginPage">
         <div className="loginArea">
-          <form className="form">
+          <form className="form" onSubmit={handleLoginSubmit}>
             <h1>Welcome to Login Page</h1>
-            <input type="text" placeholder="Username / Email" required />
-            <input type="password" placeholder="Password" required />
+            <input
+              type="text"
+              placeholder="Username / Email"
+              name="email"
+              id="emailLogin"
+              required
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              id="passwordLogin"
+              required
+            />
             <a href="">forgot password</a>
-            <button>Login</button>
+            <button type="submit">Login</button>
             <hr />
             <p>
               Don’t have an account?
